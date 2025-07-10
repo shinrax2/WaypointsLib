ee5b6afc5e72dcb051ef64c271f3acd21983550e
shinrax2 <shinra.ubu1@yahoo.de>
2025-07-10 10:57:45 +0200

    0.1

---
+++ lua/hudmanagerpd2.lua
@@ -0,0 +1,12 @@
+local init_original = HUDManager.init
+	local update_original = HUDManager.update
+
+	function HUDManager:init(...)
+		init_original(self, ...)
+		managers.waypoints:_check_pending_waypoints()
+	end
+
+	function HUDManager:update(t, dt, ...)
+		managers.waypoints:update(t, dt)
+		return update_original(self, t, dt, ...)
+	end
